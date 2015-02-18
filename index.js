f9573cbce51796eee4c494dce3191bcc9290ed21
Callum Locke <callum.locke@gmail.com>
2015-02-18 18:40:04 +0000

    use getTime for speed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = function (date) {
   date = date || new Date();
 
   var year = date.getUTCFullYear(),
+      timestamp = date.getTime(),
       day, x;
 
   // see if date is before the 1am on the last sunday in march
@@ -11,14 +12,14 @@ module.exports = function (date) {
   day = x.getUTCDay();
   if (day !== 0) x = new Date(year + '-03-' + (31 - day) + ' 01:00Z');
 
-  if (date < x) return false;
+  if (timestamp < x.getTime()) return false;
 
   // see if date is after the last sunday in october
   x = new Date(year + '-10-31 01:00Z');
   day = x.getUTCDay();
   if (day !== 0) x = new Date(year + '-10-' + (31 - day) + ' 01:00Z');
 
-  if (date >= x) return false;
+  if (timestamp >= x.getTime()) return false;
 
   // it's summer
   return true;
